c4c75d1b3a67c83baf514ca7e1a44ce88b50a5fb
SaraM <ikaqh@plaiaundi.net>
2021-01-27 21:54:39 +0100

    Drag and drop

--- js/dragDrop.js
+++ js/dragDrop.js
@@ -1,13 +1,21 @@
 function allowDrop(ev) {
     ev.preventDefault();
-}
-
-function drag(ev) {
+  }
+  
+  function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
+  }
+  
+  function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+  }
+
+/*function onDrop(ev) {
+    const id = ev.dataTransfer.getData('text');
+    const draggableElement  = document.getElementById(id);
+    const dropzone = ev.target;
+    dropzone.appendChild(draggableElement);
+    ev.dataTransfer.clearData();
 }
